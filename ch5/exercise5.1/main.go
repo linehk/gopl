83aa43b40883a54c87e403a0a976946e1ab41ffe
sulinehk <sulinehk@gmail.com>
2019-03-12 15:35:14 +0800

    ch5: fix exercise5.1

--- ch5/exercise5.1/main.go
+++ ch5/exercise5.1/main.go
@@ -1,4 +1,3 @@
-// Findlinks1 prints the links in an HTML document read from standard inpit.
 package main
 
 import (
@@ -14,20 +13,26 @@ func main() {
 		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
 		os.Exit(1)
 	}
-	visit(nil, doc)
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
 }
 
-// visit appends to links each link found in n and returns the result.
-func visit(links []string, n *html.Node) {
+func visit(links []string, n *html.Node) []string {
+	if n == nil {
+		return links
+	}
+
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
 			if a.Key == "href" {
 				links = append(links, a.Val)
-				fmt.Println(links)
 			}
 		}
 	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		visit(links, c)
-	}
+
+	links = visit(links, n.FirstChild)
+	links = visit(links, n.NextSibling)
+
+	return links
 }
