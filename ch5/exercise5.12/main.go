72e6690f7d1bb95d46bf48b71adb7579ff79a6c0
sulinehk <sulinehk@gmail.com>
2019-03-13 09:08:12 +0800

    ch5: fix exercise5.12

--- ch5/exercise5.12/main.go
+++ ch5/exercise5.12/main.go
@@ -1,8 +1,8 @@
-// Outline prints the outline of an HTML document tree.
 package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -11,12 +11,13 @@ import (
 
 func main() {
 	for _, url := range os.Args[1:] {
-		outline(url)
+		if err := outline(url); err != nil {
+			log.Println(err)
+		}
 	}
 }
 
 func outline(url string) error {
-	var depth int
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
@@ -28,13 +29,13 @@ func outline(url string) error {
 		return err
 	}
 
+	var depth int
 	startElement := func(n *html.Node) {
 		if n.Type == html.ElementNode {
 			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
 			depth++
 		}
 	}
-
 	endElement := func(n *html.Node) {
 		if n.Type == html.ElementNode {
 			depth--
@@ -47,10 +48,6 @@ func outline(url string) error {
 	return nil
 }
 
-// forEachNode calls the functions pre(x) and post(x) for each node
-// x in the tree rooted at n. Both functions are optional.
-// pre is called before the children are visited (preorder) and
-// post is called after (postorder)
 func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if pre != nil {
 		pre(n)
