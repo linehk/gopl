3a68cedd39e5e18e745ef8e7d02e880ca14a48cb
sulinehk <sulinehk@gmail.com>
2019-03-13 21:43:10 +0800

    ch6: add exercise6.5

---
+++ ch6/exercise6.5/intset.go
@@ -0,0 +1,64 @@
+// Package intset provides a set of integers based on a bit vector.
+package intset
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// 当 uint 为 64 位时，64 个 1111111... 右移 63 位变成 1，32 再左移 1 位变成 64
+// 当 uint 位 32 位时，32 个 1111111... 右移 63 位变成 0, 32 再左移 0 位 变成 32
+const uintSize = 32 << (^uint(0) >> 63)
+
+// An IntSet is a set of small non-negative integers.
+// Its zero value represents the empty set.
+type IntSet struct {
+	words []uint
+}
+
+// Has reports whether the set contains the non-negative value x.
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/uintSize, uint(x%uintSize)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add adds the non-negative value x to the set.
+func (s *IntSet) Add(x int) {
+	word, bit := x/uintSize, uint(x%uintSize)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) Unionwith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// String returns the set as a string of the form "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < uintSize; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", uintSize*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
