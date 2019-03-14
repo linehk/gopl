5307305eb52aec27a29ccd7186071193a9c06ea2
sulinehk <sulinehk@gmail.com>
2019-03-14 14:44:21 +0800

    ch7: fix exercise7.1

--- ch7/exercise7.1/main.go
+++ ch7/exercise7.1/main.go
@@ -2,51 +2,40 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
-	"strings"
 )
 
-type wlCounter int
-
-const exampleString = "Now is the winter of our discontent,\nMade glorious summer by this sun of York.\n"
-
 func main() {
-	var test wlCounter
-	wc, err := test.wordCounter(exampleString)
-	if err != nil {
-		fmt.Println(err)
-	}
+	s := "Hello, World!\nHello, 世界！"
+
+	var wc WordCounter
+	fmt.Fprintf(&wc, s)
 	fmt.Println(wc)
 
-	lc, err := test.lineCounter(exampleString)
-	if err != nil {
-		fmt.Println(err)
-	}
+	var lc LineCounter
+	fmt.Fprintf(&lc, s)
 	fmt.Println(lc)
 }
 
-func (wlc *wlCounter) wordCounter(s string) (int, error) {
-	c := 0
-	scanner := bufio.NewScanner(strings.NewReader(s))
+type WordCounter int
+
+func (c *WordCounter) Write(p []byte) (int, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(p))
 	scanner.Split(bufio.ScanWords)
 	for scanner.Scan() {
-		c++
+		*c++
 	}
-	if err := scanner.Err(); err != nil {
-		return 0, err
-	}
-	return c, nil
+	return len(p), nil
 }
 
-func (wlc *wlCounter) lineCounter(s string) (int, error) {
-	c := 0
-	scanner := bufio.NewScanner(strings.NewReader(s))
+type LineCounter int
+
+func (c *LineCounter) Write(p []byte) (int, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(p))
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		c++
+		*c++
 	}
-	if err := scanner.Err(); err != nil {
-		return 0, err
-	}
-	return c, nil
+	return len(p), nil
 }
