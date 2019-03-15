8160264963f22c6bcaea0df0f06101af991f8c1b
sulinehk <sulinehk@gmail.com>
2019-03-15 09:33:43 +0800

    ch7: fix exercise7.6

--- ch7/exercise7.6/main.go
+++ ch7/exercise7.6/main.go
@@ -1,10 +1,16 @@
-// Package tempconv performs Celsius and Fahrenheit temperature computations.
-package tempconv
+package main
 
 import (
 	"flag"
 	"fmt"
 )
+
+var temp = CelsiusFlag("temp", 20.0, "the temperature")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*temp)
+}
 
 type Celsius float64
 type Fahrenheit float64
@@ -16,17 +22,6 @@ func KToC(k Kelvin) Celsius     { return Celsius(k - 273.15) }
 
 func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
 
-/*
-package flag
-
-// Value is the interface to the value stored in a flag.
-type Value interface {
-	String() string
-	Set(string) error
-}
-*/
-
-// *celsiusFlag satisfies the flag.Value interface.
 type celsiusFlag struct{ Celsius }
 
 func (f *celsiusFlag) Set(s string) error {
@@ -47,9 +42,6 @@ func (f *celsiusFlag) Set(s string) error {
 	return fmt.Errorf("invalid temperature %q", s)
 }
 
-// CelsiusFlag defines a Celsius flag with the specified name,
-// default value, and usage, and returns the address of the flag variable.
-// The flag argument must have a quantity and a unit, e.g., "100C".
 func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
 	f := celsiusFlag{value}
 	flag.CommandLine.Var(&f, name, usage)
