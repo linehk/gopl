cc9ba5a056c3109398c44ba10f5f09f2367a5dad
sulinehk <sulinehk@gmail.com>
2019-03-21 19:45:59 +0800

    ch8: add exercise8.13

---
+++ ch8/exercise8.13/chat.go
@@ -0,0 +1,132 @@
+// Chat is a server that lets clients chat with each other.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+type client chan<- string // an outgoing message channel
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string) // all incoming client messages
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages:
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli <- msg
+			}
+
+		case cli := <-entering:
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+const timeout = 5 * time.Minute
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string)
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- ch
+
+	timer := time.NewTimer(timeout)
+	go func() {
+		<-timer.C
+		conn.Close()
+	}()
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+		timer.Reset(timeout)
+	}
+
+	leaving <- ch
+	messages <- who + " has left"
+	conn.Close()
+}
+
+/*
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // outgoing client messages
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	// talking 表示是否还在通信
+	talking := make(chan struct{})
+	go func() {
+		for {
+			if input.Scan() {
+				messages <- who + ": " + input.Text()
+				// 每发送一次消息对应 talking 的一次发收
+				talking <- struct{}{}
+			} else {
+				// input.Scan() EOF 时为 false
+				leaving <- ch
+				messages <- who + " has left"
+				conn.Close()
+				return
+			}
+		}
+	}()
+
+	for {
+		select {
+		// 未从 talking 收到值时阻塞，经过 5 分钟后退出
+		case <-talking:
+		case <-time.After(3 * time.Second):
+			conn.Close()
+			return
+		}
+	}
+
+}
+*/
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
