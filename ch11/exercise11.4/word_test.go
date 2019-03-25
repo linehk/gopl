9f3c77646417a577f872a853b3f6036f7479ec16
sulinehk <sulinehk@gmail.com>
2019-03-25 12:01:45 +0800

    ch11: exercise11.4

--- ch11/exercise11.4/word_test.go
+++ ch11/exercise11.4/word_test.go
@@ -1,69 +1,92 @@
-// Answer for Exercicse 11.1: Modify randomPalindrome to exercise
-// IsPalindrome's handling of punctuation and spaces.
-// WARNING: the conversion r -> upper -> lower doesn't preserve
-// the value of r in some cases, e.g., µ Μ, ſ S, ı I
-
-package word_test
+package word
 
 import (
 	"fmt"
 	"math/rand"
-	"os"
-	"testing"
 	"time"
-	"unicode"
+
+	"testing"
 )
 
-// IsPalindrome reports whether s reads the same forward and backward.
-// Letter case is ignored, as are non-letters.
-func IsPalindrome(s string) bool {
-	var letters []rune
-	for _, r := range s {
-		if unicode.IsLetter(r) {
-			letters = append(letters, unicode.ToLower(r))
+func TestIsPalindrome(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"detartrated", true},
+		{"A man, a plan, a canal: Panama", true},
+		{"Evil I did dwell; lewd did I live.", true},
+		{"Able was I ere I saw Elba", true},
+		{"été", true},
+		{"Et se resservir, ivresse reste.", true},
+		{"palindrome", false}, // non-palindrome
+		{"desserts", false},   // semi-palindrome
+	}
+	for _, test := range tests {
+		if got := IsPalindrome(test.input); got != test.want {
+			t.Errorf("IsPalindrome(%q) = %v", test.input, got)
 		}
 	}
-	for i := range letters {
-		if letters[i] != letters[len(letters)-1-i] {
-			return false
-		}
+}
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("A man, a plan, a canal: Panama")
 	}
-	return true
+}
+
+func ExampleIsPalindrome() {
+	fmt.Println(IsPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(IsPalindrome("palindrome"))
+	// Output:
+	// true
+	// false
 }
 
 // randomPalindrome returns a palindrome whose length and contents
 // are derived from the pseudo-random number generator rng.
-func randomNoisyPalindrome(rng *rand.Rand) string {
+func randomPalindrome(rng *rand.Rand) string {
 	n := rng.Intn(25) // random length up to 24
 	runes := make([]rune, n)
 	for i := 0; i < (n+1)/2; i++ {
-		r := rune(rng.Intn(0x200)) // random rune up to \u99
+		r := rune(rng.Intn(0x1000)) // random rune up to '\u0999'
 		runes[i] = r
-		r1 := r
-		if unicode.IsLetter(r) && unicode.IsLower(r) {
-			r = unicode.ToUpper(r)
-			if unicode.ToLower(r) != r1 {
-				fmt.Printf("cap? %c %c\n", r1, r)
-			}
-		}
 		runes[n-1-i] = r
 	}
-	return "?" + string(runes) + "!"
+	return string(runes)
 }
 
-func TestRandomNoisyPalindrome(t *testing.T) {
+func WithPuncSpace(rng *rand.Rand) string {
+	letterPuncSpace := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ,.!?　、。！？")
+	n := rng.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := letterPuncSpace[rng.Intn(len(letterPuncSpace))]
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
 	// Initialize a pseudo-random number generator.
 	seed := time.Now().UTC().UnixNano()
 	t.Logf("Random seed: %d", seed)
 	rng := rand.New(rand.NewSource(seed))
 
-	n := 0
 	for i := 0; i < 1000; i++ {
-		p := randomNoisyPalindrome(rng)
+		p := randomPalindrome(rng)
 		if !IsPalindrome(p) {
-			t.Errorf("IsNoisyPalindrome(%q) = false", p)
-			n++
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+		ps := WithPuncSpace(rng)
+		if !IsPalindrome(ps) {
+			t.Errorf("IsPalindrome(%q) = false", ps)
 		}
 	}
-	fmt.Fprintf(os.Stderr, "fail = %d\n", n)
 }
