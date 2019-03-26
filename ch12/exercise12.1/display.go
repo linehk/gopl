d86eeb8554609485529bbbe313a6bc4cdcd3340f
sulinehk <sulinehk@gmail.com>
2019-03-26 13:13:54 +0800

    ch12: add exercise12.1

---
+++ ch12/exercise12.1/display.go
@@ -0,0 +1,94 @@
+package display
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func Display(name string, x interface{}) {
+	fmt.Printf("Display %s (%T):\n", name, x)
+	display(name, reflect.ValueOf(x))
+}
+
+// formatAtom formats a value without inspecting its internal structure.
+func formatAtom(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Invalid:
+		return "invalid"
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(v.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return strconv.FormatUint(v.Uint(), 10)
+	// ...floating-point and complex cases omitted for brevity...
+	case reflect.Bool:
+		if v.Bool() {
+			return "true"
+		}
+		return "false"
+	case reflect.String:
+		return strconv.Quote(v.String())
+	case reflect.Chan, reflect.Func, reflect.Ptr, reflect.Slice, reflect.Map:
+		return v.Type().String() + "0x" +
+			strconv.FormatUint(uint64(v.Pointer()), 16)
+	default: // reflect.Array, reflect.Struct, reflect.Interface
+		return v.Type().String() + " value"
+	}
+}
+
+func display(path string, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Invalid:
+		fmt.Printf("%s = invalid\n", path)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			display(fmt.Sprintf("%s[%d]", path, i), v.Index(i))
+		}
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			display(fieldPath, v.Field(i))
+		}
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			switch key.Kind() {
+			case reflect.Array:
+				// 先收集 keys
+				var keys []string
+				for i := 0; i < key.Len(); i++ {
+					keys = append(keys, formatAtom(key.Index(i)))
+				}
+				// 再组成括号里面的值
+				arrayKey := "[" + strings.Join(keys, " ") + "]"
+				display(fmt.Sprintf("%s[%s]", path, arrayKey), v.MapIndex(key))
+			case reflect.Struct:
+				var keys []string
+				for i := 0; i < key.NumField(); i++ {
+					keys = append(keys, formatAtom(key.Field(i)))
+				}
+				structKey := "{" + strings.Join(keys, " ") + "}"
+				display(fmt.Sprintf("%s[%s]", path, structKey), v.MapIndex(key))
+			default:
+				display(fmt.Sprintf("%s[%s]", path, formatAtom(key)), v.MapIndex(key))
+			}
+		}
+	case reflect.Ptr:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			display(fmt.Sprintf("(*%s)", path), v.Elem())
+		}
+	case reflect.Interface:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			fmt.Printf("%s.type = %s\n", path, v.Elem().Type())
+			display(path+".value", v.Elem())
+		}
+	default: // basic type, channels, funcs
+		fmt.Printf("%s = %s\n", path, formatAtom(v))
+	}
+}
