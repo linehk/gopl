e688da8dc25de509a3567d8caa219e0b51c7a32d
sulinehk <sulinehk@gmail.com>
2019-03-29 13:50:12 +0800

    ch13: add exercise13.4

---
+++ ch13/exercise13.4/bzip2.go
@@ -0,0 +1,47 @@
+package bzip
+
+import (
+	"io"
+	"os/exec"
+)
+
+type writer struct {
+	in  io.WriteCloser
+	cmd *exec.Cmd
+}
+
+func NewWriter(w io.Writer) io.WriteCloser {
+	// macOS
+	cmd := exec.Command("/usr/bin/bzip2")
+
+	// 把原本发到 Stdout 的发到 w
+	cmd.Stdout = w
+
+	// 向 Stdin 写入
+	in, err := cmd.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+	// 开始
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+	wc := &writer{in, cmd}
+	return wc
+}
+
+func (w *writer) Write(data []byte) (int, error) {
+	return w.in.Write(data)
+}
+
+func (w *writer) Close() error {
+	err := w.in.Close()
+	cmdErr := w.cmd.Wait()
+	if err != nil {
+		return err
+	}
+	if cmdErr != nil {
+		return cmdErr
+	}
+	return nil
+}
