5fe5eab3534547c9bdd528ffea58f46a0c55c890
bbx-winner <41049280+bbx-winner@users.noreply.github.com>
2022-02-10 14:50:27 +0800

    Update search.go

--- ch4/github/search.go
+++ ch4/github/search.go
@@ -10,7 +10,7 @@ import (
 
 // SearchIssues queries the GitHub issue tracker.
 func SearchIssues(terms []string) (*IssuesSearchResult, error) {
-	q := url.QueryEscape(strings.Join(terms, ""))
+	q := url.QueryEscape(strings.Join(terms, " "))
 	resp, err := http.Get(IssuesURL + "?q=" + q)
 	if err != nil {
 		return nil, err
